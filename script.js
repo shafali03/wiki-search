7d5ace8cb2b37bc34f6af1350f98a7886fe605e5
shafali03 <shaf03sm@gmail.com>
2019-05-05 17:23:37 +0100

    update JS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // variable
 
 const input = document.querySelector('.main-search');
-const searchBtn = document.querySelector('main-btn');
+const searchBtn = document.querySelector('.main-btn');
 
 
 // events
@@ -11,9 +11,22 @@ searchBtn.addEventListener('click', searchWiki);
 function searchWiki(event) {
     event.preventDefault();
     showGif('show')
+    let searchValue = input.value;
+
+    const origin = 'https://en.wikipedia.org';
+    const url = `${origin}/w/api.php?action=query&origin=*&format=json&list=search&srsearch=${searchValue}`
+
+    fetch(url).then(function (data) {
+        return data.json()
+
+    }).then(displayData).catch(function (error) {
+        console.log(error);
+    })
 }
 
 // show / hide gif
+
+
 function showGif(value) {
     if (value === 'show') {
         document.querySelector('.wait-icon').classList.add('show')
@@ -22,4 +35,22 @@ function showGif(value) {
         document.querySelector('.wait-icon').classList.remove('show')
 
     }
+}
+
+// display Data
+function displayData(data) {
+    // console.log(data);
+    showGif('hide')
+    let result = data.query.search;
+    console.log(result);
+
+    let output = '';
+    result.forEach(function (item) {
+        output += ` <li class="search-item">
+                <h2 class="search-item__title">${item.title}</h2>
+                <p class="search-item__text">${item.snippet}</p>
+                <a href="http://en.wikipedia.org/?curid=${item.pageid}" class="search - item__link">read more ...</a>
+            </li > `
+    })
+    document.querySelector('.results').innerHTML = output;
 }
